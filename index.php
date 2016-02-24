8f218f0ef8fb28cd2b20904659ce79a109520c1e
ValentinIl <valentin.ilinov@gmail.com>
2016-02-24 17:19:51 +0200

    v 2.0

--- index.php
+++ index.php
@@ -1,48 +1,63 @@
 <?php
+  // $city_id = $('.city-name').val();
+  // var_dump($city_id);
+
   $link = mysqli_connect("localhost", 'root', '', 'friends');
 
-  $city = mysqli_query($link, 'SELECT id, city FROM cities limit 20');
+  $city_result = mysqli_query($link, 'SELECT id, city FROM cities limit 20');
 
-  $accommodation = mysqli_query($link, 'SELECT sourceurl, name, description, layout, features FROM accommodations WHERE city_id = 1 limit 1');
-  $restaurant = mysqli_query($link, 'SELECT sourceurl, name, description, features FROM restaurants WHERE city_id = 1 limit 1');
-  $activitiy = mysqli_query($link, 'SELECT sourceurl, name, description, layout, features FROM activities WHERE city_id = 1 limit 1');
-  $outgoing = mysqli_query($link, 'SELECT sourceurl, name, description, layout, features FROM outgoings WHERE city_id = 1 limit 1');
+  $accommodation_result = mysqli_query($link, 'SELECT sourceurl, name, description, layout, features FROM accommodations WHERE city_id = 1 limit 1');
+  $restaurant_result = mysqli_query($link, 'SELECT sourceurl, name, description, features FROM restaurants WHERE city_id = 1 limit 1');
+  $activitiy_result = mysqli_query($link, 'SELECT sourceurl, name, description, layout, features FROM activities WHERE city_id = 1 limit 1');
+  $outgoing_result = mysqli_query($link, 'SELECT sourceurl, name, description, layout, features FROM outgoings WHERE city_id = 1 limit 1');
 
-  // var_dump($city);
+  // var_dump($city_result);
   // var_dump($accommodation);
   // var_dump($restaurant);
   // var_dump($activitiy);
-  // var_dump($outgoing);
+  // var_dump($outgoing_result);
 
-  for ($i=0; $i < mysqli_num_rows($city); $i++) { 
-    $cities[$i] = mysqli_fetch_assoc($city);
+  $cities = [];
+  $final_cities = [];
+  $accommodations = [];
+  $restaurants = [];
+  $activities = [];
+  $outgoings = [];
+
+  for ($i=0; $i < mysqli_num_rows($city_result); $i++) { 
+    $cities[$i] = mysqli_fetch_assoc($city_result);
+    // $cities[$i] = mysqli_fetch_row($city_result);
+    $final_cities[$i] = $cities[$i]['city'];
+    // $final_cities[$i] = mysqli_fetch_array($city_result);
   }
-  for ($i=0; $i < mysqli_num_rows($accommodation); $i++) { 
-    $accommodations[$i] = mysqli_fetch_assoc($accommodation);
+  for ($i=0; $i < mysqli_num_rows($accommodation_result); $i++) { 
+    $accommodations[$i] = mysqli_fetch_assoc($accommodation_result);
   }
-  for ($i=0; $i < mysqli_num_rows($restaurant); $i++) { 
-    $restaurants[$i] = mysqli_fetch_assoc($restaurant);
+  for ($i=0; $i < mysqli_num_rows($restaurant_result); $i++) { 
+    $restaurants[$i] = mysqli_fetch_assoc($restaurant_result);
   }
-  for ($i=0; $i < mysqli_num_rows($activitiy); $i++) { 
-    $activities[$i] = mysqli_fetch_assoc($activitiy);
+  for ($i=0; $i < mysqli_num_rows($activitiy_result); $i++) { 
+    $activities[$i] = mysqli_fetch_assoc($activitiy_result);
   }
-  for ($i=0; $i < mysqli_num_rows($outgoing); $i++) { 
-    $outgoings[$i] = mysqli_fetch_assoc($outgoing);
+  for ($i=0; $i < mysqli_num_rows($outgoing_result); $i++) { 
+    $outgoings[$i] = mysqli_fetch_assoc($outgoing_result);
   }
 
-  // $cities[1] = mysqli_fetch_array($city);
-  // var_dump($cities[0]);
-  // $accommodations[1] = mysqli_fetch_array($accommodation);
+  // $cities[1] = mysqli_fetch_array($city_result);
+  // var_dump($final_cities);
+  // var_dump(json_encode($final_cities));
+  // $accommodations[1] = mysqli_fetch_array($accommodation_result);
   // var_dump($accommodations[0]);
-  // $restaurants[1] = mysqli_fetch_array($restaurant);
+  // $restaurants[1] = mysqli_fetch_array($restaurant_result);
   // var_dump($restaurants[0]);
-  // $activities[1] = mysqli_fetch_array($activitiy);
+  // $activities[1] = mysqli_fetch_array($activitiy_result);
   // var_dump($activities[0]);
-  // $outgoings[1] = mysqli_fetch_array($outgoing);
+  // $outgoings[1] = mysqli_fetch_array($outgoing_result);
   // var_dump($outgoings[0]);
 
-
   mysqli_close($link);
+
+  // header('Location: template.php');
 
   require_once "template.php";
 ?>
--- template.php
+++ template.php
@@ -4,27 +4,29 @@
   <meta charset="UTF-8">
   <title>City</title>
   <link href="bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="jquery-ui.min.css">
+  <link rel="stylesheet" href="jquery-ui.theme.min.css">
 </head>
 <body>
   <div class="container">
-    <select name="city" id="">
-      <?php
-        foreach($cities as $city) {
-          echo '<option value="' . $city['id'] . '">'. $city['city'] . '</option>';
-        }
-      ?>
-    </select>
+    <form action="" class="form form-default">
+<!--       <select name="city" class="city-name">
+        <?php
+          foreach($cities as $city_result) {
+            echo '<option value="' . $city_result['id'] . '">'. $city_result['city'] . '</option>';
+          }
+        ?>
+      </select> -->
 
-    <div>
-      <input class="city-autocomplete"type="text" name="city" placeholder="Input city, please">
-    </div>
+      <div>
+        <input class="city-autocomplete" type="text" name="city" placeholder="Input city, please">
+      </div>
+      <input type="submit" value="Submit">
+    </form>
 
-    <h1>Amsterdam</h1>
+    <!-- <h1><?=$city_result['city'];?></h1> -->
+    <h1 class="title"></h1>
 
-    <h1><?=$city['city'];?></h1>
-    <h1><?php
-      var_dump($cities[0]['city']);
-    ?></h1>
     <table class="table table-striped">
     <colgroup width="100">
     <colgroup width="300">
@@ -37,49 +39,66 @@
         <th>Features</th>
     </tr>
     
-    <?php foreach($accommodations as $accommodation) {  ?>
+    <?php foreach($accommodations as $accommodation_result) {  ?>
       <tr>
-        <td><?=$accommodation['name'];?></td>
-        <td><?=$accommodation['description'];?></td>
-        <td><?=$accommodation['layout'];?></td>
-        <td><?=$accommodation['features'];?></td>
+        <td><?=$accommodation_result['name'];?></td>
+        <td><?=$accommodation_result['description'];?></td>
+        <td><?=$accommodation_result['layout'];?></td>
+        <td><?=$accommodation_result['features'];?></td>
       </tr>
     <?php  }  ?>
-    <?php foreach($restaurants as $restaurant) {  ?>
+    <?php foreach($restaurants as $restaurant_result) {  ?>
       <tr>
-        <td><?=$restaurant['name'];?></td>
-        <td><?=$restaurant['description'];?></td>
+        <td><?=$restaurant_result['name'];?></td>
+        <td><?=$restaurant_result['description'];?></td>
         <td></td>
-        <td><?=$restaurant['features'];?></td>
+        <td><?=$restaurant_result['features'];?></td>
       </tr>
     <?php  }  ?>
-    <?php foreach($activities as $activity) {  ?>
+    <?php foreach($activities as $activity_result) {  ?>
       <tr>
-        <td><?=$activity['name'];?></td>
-        <td><?=$activity['description'];?></td>
-        <td><?=$activity['layout'];?></td>
-        <td><?=$activity['features'];?></td>
+        <td><?=$activity_result['name'];?></td>
+        <td><?=$activity_result['description'];?></td>
+        <td><?=$activity_result['layout'];?></td>
+        <td><?=$activity_result['features'];?></td>
       </tr>
     <?php  }  ?>
-    <?php foreach($outgoings as $outgoing) {  ?>
+    <?php foreach($outgoings as $outgoing_result) {  ?>
       <tr>
-        <td><?=$outgoing['name'];?></td>
-        <td><?=$outgoing['description'];?></td>
-        <td><?=$outgoing['layout'];?></td>
-        <td><?=$outgoing['features'];?></td>
+        <td><?=$outgoing_result['name'];?></td>
+        <td><?=$outgoing_result['description'];?></td>
+        <td><?=$outgoing_result['layout'];?></td>
+        <td><?=$outgoing_result['features'];?></td>
       </tr>
     <?php  }  ?>
     </table>
   </div>
 
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <!--[if lt IE 9]>
+      <script src="jquery-1.12.0.min.js"></script>
+    <![endif]-->
+    <!--[if gte IE 9]><!-->
+      <script src="jquery-2.2.0.min.js"></script>
+    <!--[endif]-->
+    <script src="jquery-ui.min.js"></script>
+
   <script>
-    <?php
-      echo "var final_cities = city['" . $city['city'] . "'];";
-    ?>
+    $(function() {
+      var final_cities = <?=json_encode($final_cities);?>;
 
+      $(".city-autocomplete").autocomplete({
+        source: final_cities
+      });
 
-    $( ".city-autocomplete" ).autocomplete({
-      source: final_cities
+        $('.form').on('submit', function() {
+          // console.log($('.city-name').val());
+          // console.log($('.city-autocomplete').val());
+          $('.title').html($('.city-autocomplete').val());
+          
+
+          return false
+        })
     });
   </script>
 
